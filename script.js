d80191b5ba63057c9ee5aa00bb7992504f3e10da
cb040719 <claudiob0162@gmail.com>
2022-10-09 21:39:48 +0200

    added dynamic html return

--- script.js
+++ script.js
@@ -1,35 +1,68 @@
 const $mainSection = document.querySelector(".mainSection");
 const $workbenchSection = document.querySelector(".workbenchSection");
-const $newCard = document.querySelector("#newCard");
-const $newImgCard = document.querySelector("newImgCard");
-const $newHeader = document.querySelector("newHeader");
+const $codeSection = document.querySelector(".codeSection");
 
-let workBench = [];
+let workbench = [];
+let generatedHTML = [];
+
 
 $mainSection.addEventListener("click", function(event) {
     
     if(event.target.id === "newCard") {
-
-        console.log(event.target.id);
-
-        // workBench = [];
+        console.log(event.target.id);   
         
-        workBench.push(`
-            <div class="aCard">
-                <h4>newCard</h4>
-                <p>the card text</p>
-            <div/>
+        workbench.push(`
+        <div class="aCard">
+            <h2>Card Title</h2>
+            <p>Card Text</p>
+            <button id = "getCardCode">Get the Code</button>
+        </div>
         `);
-
-        $workbenchSection.innerHTML = workBench.join("");
     };
     
     if(event.target.id === "newImgCard") {
         console.log(event.target.id);
+        
+        workbench.push(`
+        <div class="aImgCard">
+            <h2>Card Title</h2>
+            <img src="https://picsum.photos/200" alt="img"/>
+            <p>Card Text</p>
+            <button id = "getImgCardCode">Get the Code</button>
+        </div>
+        `);
     };
-
+    
     if(event.target.id === "newHeader") {
         console.log(event.target.id);
     };
 
+    if(event.target.id === "clearBTN") {
+        console.log(event.target.id);
+
+        workbench = [];
+        console.clear();
+    };
+
+    
+    $workbenchSection.innerHTML = workbench.join("");
 });
+
+$workbenchSection.addEventListener("click", function(event) {
+    
+    if(event.target.id === "getImgCardCode") {
+        $codeSection.innerHTML = '';
+        generatedHTML = [];
+        $imgCard = document.querySelector(".aImgCard");
+        generatedHTML.push($imgCard.innerHTML);
+    }
+
+    if(event.target.id === "getCardCode") {
+        $codeSection.innerHTML = '';
+        generatedHTML = [];
+        $card = document.querySelector(".aCard");
+        generatedHTML.push($card.innerHTML);
+    }
+    
+    $codeSection.append(generatedHTML);
+})
